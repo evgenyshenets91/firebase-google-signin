28810f8928f86bb90711f3dfbae13f03514f703f
Evgeny Shenets <evgenyshenets91@gmail.com>
2021-09-26 20:19:00 +0300

    add auth package and check google sign in iOS

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import {Button, SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId:
+    '362081977337-hsqgab52ddhlf1i2d788cqn3hv576nq8.apps.googleusercontent.com',
+});
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -10,9 +18,30 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const onGoogleButtonPress = async () => {
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    console.log('idToken', idToken);
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  };
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Button
+        title="Google Sign-In"
+        onPress={() =>
+          onGoogleButtonPress().then(() =>
+            console.log('Signed in with Google!'),
+          )
+        }
+      />
     </SafeAreaView>
   );
 };
